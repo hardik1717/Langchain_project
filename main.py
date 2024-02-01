b7da28e33886d1e95abb4321e6dcfbe3dc9b6827
Hardik Kanekar <101920338+hardik1717@users.noreply.github.com>
2024-02-01 13:23:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from Langchain_code import get_few_shot_db_chain
+
+import streamlit as st
+
+st.title("T shirts : Database Q & A 👕")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    response = chain.run(question)
+    st.header("Answer: ")
+    st.write(response)
